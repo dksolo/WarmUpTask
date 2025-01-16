c24e02eae26d1ae099206eb193994e10a261d2e7
DKSolo <dbm95mail@gmail.com>
2025-01-16 21:15:56 +0500

    feat: Добавлен footer

--- src/containers/Footer/Footer.tsx
+++ src/containers/Footer/Footer.tsx
@@ -1,12 +1,18 @@
 import clsx from "clsx";
 import styles from './TemplateName.module.scss';
+import Anchor from '../../components/Anchor/Anchor';
 
 export type FooterProps = {
 	className?: string;
 };
 
-export function TemplateName({ className }: TemplateNameProps) {
-	return <div className={clsx(styles.container, className)} data-testid="TemplateName">
-		TemplateName
+export default function Footer({ className }: FooterProps) {
+	return <div className={clsx(styles.container, className)}>
+		<div>Logo</div>
+		<nav>
+			<li><Anchor type={'href'} href={'/'} text={'Home'} /></li>
+			<li><Anchor type={'href'} href={'/login'} text={'Login'} /></li>
+			<li><Anchor type={'href'} href={'/register'} text={'Register'} /></li>
+		</nav>
 	</div>;
 }

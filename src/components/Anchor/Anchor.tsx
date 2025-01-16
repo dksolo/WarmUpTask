ad60c1fc2878100c949ff7f2d1ee826ef0fdea10
DKSolo <dbm95mail@gmail.com>
2025-01-16 21:07:35 +0500

    fix: Anchor; feat: Header

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -1,25 +1,27 @@
 import clsx from "clsx";
 import styles from './Anchor.module.scss';
-import { NavLink, To } from 'react-router';
+import { To, NavLink } from 'react-router';
 
 export type AnchorProps = {
-	type: 'navLink' | 'href';
+	type: 'to' | 'href';
 	className?: string;
 	href?: string;
 	to?: To;
 	text: string;
 }
 
-export function Anchor({ type, className, href, to, text }: AnchorProps) {
+const Anchor: React.FC<AnchorProps> = ({ type, className, to, href, text }) => {
 	let component: React.ReactNode;
 	switch (type) {
-		case "navLink":
+		case "to":
 			component = <NavLink className={clsx(styles.container, className)} to={to!}>{text}</NavLink>;
 			break;
 		case "href":
 			component = <a className={clsx(styles.container, className)} href={href!}>{text}</a>;
 			break;
-	}
 
+	}
 	return component;
 }
+
+export default Anchor
--- src/containers/Header/Header.tsx
+++ src/containers/Header/Header.tsx
@@ -1,14 +1,20 @@
 import clsx from "clsx";
 import styles from './TemplateName.module.scss';
+import Anchor from '../../components/Anchor/Anchor';
+
+
 
 export type HeaderProps = {
 	className: string;
-	children?: React.ReactNode;
 };
 
-export default function Header({ className }: TemplateNameProps) {
+export default function Header({ className }: HeaderProps) {
 	return <div className={clsx(styles.container, className)}>
-
-		{children}
+		<div>Logo</div>
+		<nav>
+			<li><Anchor type={'href'} href={'/'} text={'Home'} /></li>
+			<li><Anchor type={'href'} href={'/login'} text={'Login'} /></li>
+			<li><Anchor type={'href'} href={'/register'} text={'Register'} /></li>
+		</nav>
 	</div>;
 }

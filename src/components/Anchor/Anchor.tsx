88f7d5abb2ee90e1a8691781f25a47a0e2c41200
DKSolo <dbm95mail@gmail.com>
2025-01-18 15:41:00 +0500

    fix: Anchor Simplified

--- src/components/Anchor/Anchor.tsx
+++ src/components/Anchor/Anchor.tsx
@@ -3,23 +3,19 @@ import styles from './Anchor.module.scss';
 import { To, NavLink } from 'react-router';
 
 export type AnchorProps = {
-	type: 'to' | 'href';
 	className?: string;
 	href?: string;
 	to?: To;
 	text: string;
 }
 
-const Anchor: React.FC<AnchorProps> = ({ type, className, to, href, text }) => {
+const Anchor: React.FC<AnchorProps> = ({ className, to, href, text }) => {
 	let component: React.ReactNode;
-	switch (type) {
-		case "to":
-			component = <NavLink className={clsx(styles.container, className)} to={to!}>{text}</NavLink>;
-			break;
-		case "href":
-			component = <a className={clsx(styles.container, className)} href={href!}>{text}</a>;
-			break;
+	if (to) {
+		component = <NavLink className={clsx(styles.container, className)} to={to!}>{text}</NavLink>;
 
+	} else {
+		component = <a className={clsx(styles.container, className)} href={href!}>{text}</a>;
 	}
 	return component;
 }

5b0c9e1de1c43b2be2fcb4abd3786bbea609eb8b
DKSolo <dbm95mail@gmail.com>
2025-01-18 16:06:53 +0500

    feat: Typograhy

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -2,12 +2,14 @@ import clsx from "clsx";
 import styles from './Typography.module.scss';
 
 export type TypographyProps = {
-	type: 'title' | 'text';
+	tag: string;
 	value: string;
+	className?: string;
 };
 
-export function Typography({ type, value }: TypographyProps) {
-	return <div className={clsx(styles.container, className)} data-testid="TemplateName">
-		TemplateName
-	</div>;
+export function Typography({ tag, value, className }: TypographyProps) {
+	const CustomTag = tag as keyof JSX.IntrinsicElements;
+	return <CustomTag className={clsx(styles.container, className)}>
+		{value}
+	</CustomTag>;
 }

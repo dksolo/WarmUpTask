4b7f8c302bebe31c463ab7002079707dc0ca49de
DKSolo <dbm95mail@gmail.com>
2025-01-18 19:56:26 +0500

    feat: Action

--- src/components/Action/Action.tsx
+++ src/components/Action/Action.tsx
@@ -3,9 +3,22 @@ import styles from './TemplateName.module.scss';
 
 export type ActionProps = {
 	type: 'button' | 'link';
+	href: string;
+	text: string;
+	className?: string;
 };
 
-export function TemplateName({ className }: TemplateNameProps) {
+export function Action({ type, href, text, className, ...props }: ActionProps) {
+	var component: React.ReactNode;
+	switch (type) {
+		case 'button':
+			component = <button className={clsx(styles.container, className)} {...props}>{text}</button>;
+			break;
+		case 'link':
+
+			component = <a className={clsx(styles.container, className)} href={href}>{text}</a>;
+			break;
+	}
 	return <div className={clsx(styles.container, className)} data-testid="TemplateName">
 		TemplateName
 	</div>;
